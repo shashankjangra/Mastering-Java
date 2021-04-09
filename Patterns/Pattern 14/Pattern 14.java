3f43b21048bd065d1cb5a9f90c0e75ca53151244
Shashank Jangra <57314546+shashankjangra@users.noreply.github.com>
2021-04-09 10:56:50 +0530

    Add files via upload

---
+++ Patterns/Pattern 14/Pattern 14.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int n = input.nextInt();
+        
+        for (int i=1; i<=10; i++) {
+            System.out.println(n + " * " + i + " = " + (n*i));
+        }
+    }
+}
