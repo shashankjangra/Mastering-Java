7077d6beb2ace3920f2560ae6849645db194352a
Shashank Jangra <jangra.shashank@gmail.com>
2021-06-09 22:08:13 +0530

    13 pattern

---
+++ 01 Basics of Programming/02 Patterns/Pattern 13/Pattern 13.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class Main{
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        int n = input.nextInt();
+        
+        for(int i=0; i<n; i++) {
+            int icj = 1;
+            for(int j=0; j<=i; j++) {
+                System.out.print(icj + "	");
+                int icjp1 = ( icj * (i-j) ) / (j+1);
+                icj = icjp1;
+            }
+            System.out.println();
+        }
+    }
+}
